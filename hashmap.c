df9d263779c6ba609ec0dbee8f1da5cefebecdbc
AlexandraJMV <>
2022-04-09 04:48:18 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -146,6 +146,13 @@ Pair * firstMap(HashMap * map) {
 }
 
 Pair * nextMap(HashMap * map) {
+	long *pos = &(map->current);
 
-    return NULL;
+	while (map->buckets[*pos] == NULL || map->buckets[*pos]->key == NULL)
+	{
+		(*pos) = ((*pos) + 1)%(map->capacity);
+		if (*pos == 0) return NULL;
+	}
+
+    return map->buckets[*pos];
 }
